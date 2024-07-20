956c5f284c197e87566fc1c019927f7d4d62b96f
Tiago França <tiago@tiagofranca.com>
2024-07-20 12:23:08 -0300

    gh-pages start config
    On branch WIP-feat/gh-pages-with-vite-build
    	new file:   .github/workflows/deploy.yml
    	modified:   .gitignore
    	modified:   package.json
    	new file:   vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,4 @@
+export default {
+    // TODO: usar as configs do vite para identificar o 'env' e mudar o 'base' e outras configs de acordo
+    base: '/Another-WebRTC-Screen-Recorder/', // Usado para o Github Pages
+};
